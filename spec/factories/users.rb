99380029cda9f37124559ed3c0b4ac4324277c61
tomo111111 <ttt0206sss@gmail.com>
2020-08-31 15:45:18 +0900

    モデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    store_name {"みかん店"}
+    store_number {"123456"}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 8)
+    password {password}
+    password_confirmation {password}
+  end
+end
