9a7e2512c54bbf071813d64998a34ead7d921808
tomo111111 <ttt0206sss@gmail.com>
2020-09-04 18:37:46 +0900

    新規アイテム登録・削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,27 @@ class ItemsController < ApplicationController
     @starting_point = Date.today - 7
     @items = Item.all
   end
+
+  def new
+    @items = Item.all
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to new_item_path
+  end
+
+  def destroy
+    item = Item.find(params[:name])
+    item.destroy
+    redirect_to new_item_path
+  end
+
+private
+
+def item_params
+  params.require(:item).permit(:name,:margin).merge(user_id:current_user.id)
+end
+
 end
