b4fd96dad7811850c83cf3309df9343e7033a710
tomo111111 <ttt0206sss@gmail.com>
2020-09-20 22:36:09 +0900

    itemモデルテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :item do
+    name  {"おいしいパン"}
+    margin{8}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe 'Itemの保存' do
+    context "保存できる場合" do
+      it "name,marginがあり、marginが整数である" do
+        expect(@item).to be_valid
+      end
+    end
+    context "保存できない場合" do
+      it "nameが空だと保存できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "marginが空だと保存できない" do
+        @item.margin = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Margin can't be blank")
+      end
+      it "marginが整数以外だと保存できない" do
+        @item.margin = 2.2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Margin must be an integer")
+      end
+    end
+  end
 end
